04450ad629405e100fcd11ee13665e46c4ef0696
nangsaigon <binickname@gmail.com>
2016-07-05 21:36:34 +0700

    added Jersey client

---
+++ Java-RESTful-Client-Example/src/main/java/com/howtoprogram/service/BookRepositoryImplJersey.java
@@ -0,0 +1,69 @@
+package com.howtoprogram.service;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.howtoprogram.domain.Book;
+
+public class BookRepositoryImplJersey {
+  private static final String URI_BOOK = "http://localhost:8080/v1/books";
+
+  public void deleteBook(Long id) {
+    Client client = ClientBuilder.newClient();
+    WebTarget target = client.target(URI_BOOK).path(String.valueOf(id));
+    Response response = target.request().delete();
+    System.out.println("Status code:" + response.getStatus());
+  }
+
+  public static void main(String[] args) throws Exception {
+    BookRepositoryImplJersey bookRepository = new BookRepositoryImplJersey();
+    // Getting the first book from the RESTful service
+    Book book = bookRepository.getAllBooks()[0];
+    bookRepository.deleteBook(book.getId());
+
+  }
+
+  public Book updateBook(Book book) throws Exception {
+    Client client = ClientBuilder.newClient();
+    WebTarget target = client.target(URI_BOOK).path(String.valueOf(book.getId()));
+    Response response = target.request(MediaType.APPLICATION_JSON_TYPE)
+        .put(Entity.entity(book, MediaType.APPLICATION_JSON_TYPE));
+    int status = response.getStatus();
+    System.out.println("Status code: " + status);
+    Book createdBook = response.readEntity(Book.class);
+    return createdBook;
+  }
+
+
+  public Book createBook(Book book) throws Exception {
+    Client client = ClientBuilder.newClient();
+    Response response = client.target(URI_BOOK).request()
+        .post(Entity.entity(book, MediaType.APPLICATION_JSON_TYPE));
+    int status = response.getStatus();
+    System.out.println("Status code: " + status);
+    Book createdBook = response.readEntity(Book.class);
+    return createdBook;
+
+  }
+
+
+
+  public Book[] getAllBooks() throws Exception {
+    Client client = ClientBuilder.newClient();
+    Response response = client.target(URI_BOOK).request().get();
+    int status = response.getStatus();
+    System.out.println("Status code: " + status);
+    Book[] books = response.readEntity(Book[].class);
+    return books;
+  }
+
+
+  public Book findBookById(Long id) {
+    return null;
+  }
+
+}
