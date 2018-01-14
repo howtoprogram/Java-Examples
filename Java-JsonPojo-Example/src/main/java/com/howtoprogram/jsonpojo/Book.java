e7cc7651044ceda2b59ea3315e5cdf249935af0a
Unknown <binickname@gmail.com>
2018-01-14 23:39:30 +0900

    update pojo

--- Java-JsonPojo-Example/src/main/java/com/howtoprogram/jsonpojo/Book.java
+++ Java-JsonPojo-Example/src/main/java/com/howtoprogram/jsonpojo/Book.java
@@ -1,75 +1,87 @@
 package com.howtoprogram.jsonpojo;
 
+import java.time.LocalDate;
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 public class Book {
 
-  private Long id;
-  private String name;
-  private String author;
+	private Long id;
+	private String name;
+	private String author;
 
+	@JsonInclude(NON_NULL)
+	private LocalDate pubDate;
+	@JsonInclude(NON_NULL)
+	private Date copyrightDate;
 
-  public Book() {
-    super();
-  }
+	public Book() {
+		super();
+	}
 
-  public Book(Long id, String name, String author) {
-    super();
-    this.id = id;
-    this.name = name;
-    this.author = author;
-  }
+	public Book(Long id, String name, String author) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.author = author;
+	}
 
-  /**
-   * @return the id
-   */
-  public Long getId() {
-    return id;
-  }
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
 
-  /**
-   * @param id the id to set
-   */
-  public void setId(Long id) {
-    this.id = id;
-  }
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-  /**
-   * @return the name
-   */
-  public String getName() {
-    return name;
-  }
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
 
-  /**
-   * @param name the name to set
-   */
-  public void setName(String name) {
-    this.name = name;
-  }
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
 
-  /**
-   * @return the author
-   */
-  public String getAuthor() {
-    return author;
-  }
+	/**
+	 * @return the author
+	 */
+	public String getAuthor() {
+		return author;
+	}
 
-  /**
-   * @param author the author to set
-   */
-  public void setAuthor(String author) {
-    this.author = author;
-  }
+	/**
+	 * @param author
+	 *            the author to set
+	 */
+	public void setAuthor(String author) {
+		this.author = author;
+	}
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see java.lang.Object#toString()
-   */
-  @Override
-  public String toString() {
-    return "Book [id=" + id + ", name=" + name + ", author=" + author + "]";
-  }
-
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", name=" + name + ", author=" + author + "]";
+	}
 
 }
